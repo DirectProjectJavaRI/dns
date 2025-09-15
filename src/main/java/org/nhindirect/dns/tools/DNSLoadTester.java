81fcbcd9514d9756f9dcf9c2d4e1705a088cc8e7
greg.meyer <greg.meyer@oracle.com>
2025-09-15 07:18:17 -0500

    Cleaning up compiler warnings.

--- src/main/java/org/nhindirect/dns/tools/DNSLoadTester.java
+++ src/main/java/org/nhindirect/dns/tools/DNSLoadTester.java
@@ -11,7 +11,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.xbill.DNS.Cache;
