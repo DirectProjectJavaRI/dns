81f4460c08f9c1f92e1c28d9d38eb5a527f08b77
Greg Meyer <abareasol@gmail.com>
2026-01-08 07:07:46 -0600

    Updating to SpringBoot 4.

--- src/test/java/org/nhindirect/dns/TestApplication.java
+++ src/test/java/org/nhindirect/dns/TestApplication.java
@@ -2,17 +2,10 @@ package org.nhindirect.dns;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(exclude= {SecurityAutoConfiguration.class, 
-		ReactiveSecurityAutoConfiguration.class})
-@ComponentScan({"org.nhindirect.config"})
-@Import(HttpMessageConvertersAutoConfiguration.class)
+
+@SpringBootApplication
 public class TestApplication
 {
     public static void main(String[] args) 
